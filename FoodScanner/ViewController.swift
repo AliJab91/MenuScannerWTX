b64753f83e813a75ea1b12d27fabb4e48866969d
Ali Jaber <ai_jr_91@hotmail.com>
2019-04-14 17:16:56 +0300

    Initial Commit

---
+++ FoodScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodScanner
+//
+//  Created by Ali Jaber on 4/14/19.
+//  Copyright © 2019 Ali Jaber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
